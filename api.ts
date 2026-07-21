65491017091817026cf07cbeb28b4dddd9cc80b5
NarelCurupira <narelcurupira@gmail.com>
2026-07-21 09:45:13 -0300

    Delete api.ts

--- api.ts
+++
@@ -1,8 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
-  <defs><linearGradient id="g" x1="32" y1="24" x2="224" y2="232" gradientUnits="userSpaceOnUse"><stop stop-color="#2d7db3"/><stop offset="1" stop-color="#102a43"/></linearGradient></defs>
-  <rect width="256" height="256" rx="54" fill="url(#g)"/>
-  <path d="M66 63h124a14 14 0 0 1 14 14v114a14 14 0 0 1-14 14H66a14 14 0 0 1-14-14V77a14 14 0 0 1 14-14Z" fill="#fff" opacity=".96"/>
-  <path d="M82 100h92M82 132h92M82 164h56" stroke="#2d7db3" stroke-width="13" stroke-linecap="round"/>
-  <circle cx="174" cy="164" r="24" fill="#2a9d8f"/>
-  <path d="m163 164 8 8 15-17" fill="none" stroke="#fff" stroke-width="7" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
