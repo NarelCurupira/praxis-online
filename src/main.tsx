63afcab7021fadd87a00c2448ea90029216f8add
NarelCurupira <narelcurupira@gmail.com>
2026-09-11 01:06:34 -0300

    Versão 1.0-RC4

--- src/main.tsx
+++ src/main.tsx
@@ -17,6 +17,7 @@ import "./praxis1-tokens.css";
 import "./praxis1-shell.css";
 import "./praxis1-entry.css";
 import "./praxis1-home.css";
+import "./praxis1-workflow.css";
 
 const App = lazy(() => import("./App"));
 
